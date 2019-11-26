508fb7fbabe08b25b990d870b4775d67b5984ca3
TakoNya <camilabelentaraborelli2@gmail.com>
2019-11-25 21:51:50 -0300

    LongitudCadena

---
+++ src/longitudcadena/LongitudCadena.java
@@ -0,0 +1,11 @@
+package longitudcadena;
+
+public class LongitudCadena {
+
+        public static void main(String[] args) {
+            String cadena = "Annyeong hasaeyo jonun bangtan sonyeondanun hwangun maknae jeon jungkook ibnida";
+            System.out.println("La cadena tiene " + cadena.length() + " caracteres");
+        
+    }
+    
+}
